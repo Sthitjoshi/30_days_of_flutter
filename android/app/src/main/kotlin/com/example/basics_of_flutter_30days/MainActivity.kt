8d2f828aa822687d954072d386e255830c0a6e65
Sthitjoshi <sthitjoshi@gmail.com>
2023-11-10 14:39:28 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/basics_of_flutter_30days/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basics_of_flutter_30days
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days =30;
+  final String name ="Sthita";
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title:Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days flutter with $name"),
+        )
+      ),
+    );
+  }
+}
