1d77cdb1157cd5e7cec4f4155edca85a26f27c49
Sthitjoshi <sthitjoshi@gmail.com>
2023-11-18 17:09:23 +0530

    day 7 completed(all about themes)

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -14,7 +14,7 @@ class Item {
 
 final products = [
   Item(
-      id: "Codepur001",
+      id: "sthita007",
       name: "iPhone 12 Pro",
       desc: "Apple iPhone 12th generation",
       price: 999,
